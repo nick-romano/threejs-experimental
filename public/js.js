7d88dcb607850bdd95c8df15e3c3c5f2d2c9c8c4
nick-romano <naromano22@gmail.com>
2017-07-30 13:27:14 -0400

    added different color sides to a cube

--- public/js.js
+++ public/js.js
@@ -5,12 +5,31 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
+
+
+var materials = [
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 }),        // Left side
+    new THREE.MeshBasicMaterial({ color: 0x0B5394 }),       // Right side
+    new THREE.MeshBasicMaterial({ color: 0x62643D }),         // Top side
+    new THREE.MeshBasicMaterial({ color: 0x077E24}),      // Bottom side
+    new THREE.MeshBasicMaterial({ color: 0x2D3553 }),       // Front side
+    new THREE.MeshBasicMaterial({ color: 0x7F16B1 })         // Back side
+];
+var geometry = new THREE.BoxGeometry(25,25,25);
+var cube = new THREE.Mesh(geometry, materials);
 scene.add(cube);
 
-camera.position.z = 5;
+
+
+
+// var frame1 = new THREE.Mesh(
+// 	new THREE.BoxGeometry(30,30,10)
+// 	);
+// scene.add(frame1)
+
+
+
+camera.position.z = 55;
 
 var controls = new THREE.TrackballControls(camera);
 
