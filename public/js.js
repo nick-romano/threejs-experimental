91db4b311d517e30d43339b44a893f86b1f23632
countrytoast <naromano22@gmail.com>
2017-07-30 12:29:16 -0400

    some updates, basic cube scene

--- public/js.js
+++ public/js.js
@@ -1,64 +1,23 @@
-/*Most of this code was written and or adopted  from Bjørn Sandvik's blog: 
-http://blog.mastermaps.com/2013/09/creating-webgl-earth-with-threejs.html*/
-
-var webGL = $('#three');
-
-
-var width = window.innerWidth,
-    height = window.innerHeight;
-
-
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
-camera.position.z = 1.5;
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(width, height);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-scene.add(new THREE.AmbientLight(0x333333));
-var light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(5,3,5);
-scene.add(light);
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-var sphere = new THREE.Mesh(
-			new THREE.SphereGeometry(0.5, 64, 64),
-			new THREE.MeshPhongMaterial({
-				map:         THREE.ImageUtils.loadTexture('2_no_clouds_4k_BW.jpg'),
-				bumpMap:     THREE.ImageUtils.loadTexture('elev_bump_4k.jpg'),
-				bumpScale:   0.005,
-				specularMap: THREE.ImageUtils.loadTexture('water_4k.png'),
-				specular:    new THREE.Color('grey')								
-			})
-		);
-scene.add(sphere);
-
-
-var clouds = new THREE.Mesh(
-  new THREE.SphereGeometry(0.503, 64, 64),
-  new THREE.MeshPhongMaterial({
-    map: THREE.ImageUtils.loadTexture('fair_clouds_4k.png'),
-    transparent: true
-  })
-);
-scene.add(clouds);
-
-var stars = new THREE.Mesh(
-  new THREE.SphereGeometry(90, 64, 64), 
-  new THREE.MeshBasicMaterial({
-    map: THREE.ImageUtils.loadTexture('galaxy_starfield.png'), 
-    side: THREE.BackSide
-  })
-);
-scene.add(stars);
+camera.position.z = 5;
 
 var controls = new THREE.TrackballControls(camera);
-webGL.append(renderer.domElement);
-render();
 
-function render() {
-  controls.update();
-  sphere.rotation.y += 0.0005;
-  clouds.rotation.y += 0.0005;  
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
-}
+var animate = function() {
+	controls.update();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+};
+
+animate();
